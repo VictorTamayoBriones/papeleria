637d28501cf33bf0c137745ba422ed70fb6e104c
victor640 <54726734+victor640@users.noreply.github.com>
2020-08-17 13:34:15 -0500

    newconection

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-$host="localhost";
-$user="id14604783_papeleriauser";
-$pass='JLSU%&tMVgCr#!KlxjG6';
-$database='id14604783_papeleriatlaxcaltecautt';
+$host="us-cdbr-east-02.cleardb.com";
+$user="bb58de1f288001";
+$pass='d7e56db0';
+$database='heroku_b270f84ba5aa023';
 
 $conexion= mysqli_connect($host,$user,$pass,$database);
 
@@ -11,6 +11,16 @@ $conexion= mysqli_connect($host,$user,$pass,$database);
 //m$$qKB(pH9fOjy>*
 //JLSU%&tMVgCr#!KlxjG6
 //iniciar la session
+
+
+
+//bb58de1f288001
+
+//d7e56db0
+
+//us-cdbr-east-02.cleardb.com
+
+
 
 session_start();
 
