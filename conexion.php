b36202e5618feaff19bffb2b17e6470694d660d2
victor640 <54726734+victor640@users.noreply.github.com>
2020-08-17 13:29:25 -0500

    cambiar despues

--- conexion.php
+++ conexion.php
@@ -1,8 +1,14 @@
 <?php
 
-$conexion= mysqli_connect("sql313.tonohost.com","ottos_26495925","0y86zxng","ottos_26495925_papeleriaTlaxcalteca");
+$host="localhost";
+$user="id14604783_papeleriauser";
+$pass='JLSU%&tMVgCr#!KlxjG6';
+$database='id14604783_papeleriatlaxcaltecautt';
 
+$conexion= mysqli_connect($host,$user,$pass,$database);
 
+//m$$qKB(pH9fOjy>*
+//JLSU%&tMVgCr#!KlxjG6
 //iniciar la session
 
 session_start();
