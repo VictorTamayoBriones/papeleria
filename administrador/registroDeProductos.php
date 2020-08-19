020ab56c0a0a60cfbb1c21da32f7df6706587209
victor640 <54726734+victor640@users.noreply.github.com>
2020-08-19 16:25:53 -0500

    k

--- administrador/registroDeProductos.php
+++ administrador/registroDeProductos.php
@@ -26,9 +26,9 @@ if(isset($_POST['registrar'])){
             header ("location: regisProduct.php?exito");
         }
         else{
-            ?>
             
-            <?php
+                header ("location: regisProduct.php?error");
+            
         }
 
     }
