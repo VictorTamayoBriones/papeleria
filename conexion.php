40f5617c0e777bd488640ce243633ed595f0819e
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2020-08-19 17:49:16 -0500

    aa
    aa

--- conexion.php
+++ conexion.php
@@ -1,12 +1,12 @@
 <?php
 
-$host="us-cdbr-east-02.cleardb.com";
+/*$host="us-cdbr-east-02.cleardb.com";
 $user="bb58de1f288001";
 $pass='d7e56db0';
 $database='heroku_b270f84ba5aa023';
 
-$conexion= mysqli_connect($host,$user,$pass,$database);
-
+$conexion= mysqli_connect($host,$user,$pass,$database);*/
+$conexion= mysqli_connect("localhost", "root", "", "papeleria");
 
 //m$$qKB(pH9fOjy>*
 //JLSU%&tMVgCr#!KlxjG6
